26ef93d67e924925ccbd6f685bc8190410cb2fe5
tarik <tarik.kich@hotmail.fr>
2016-04-17 16:29:22 -0700

    [ClientApp] convert indentation into spaces

--- ClientApp/src/All_Client/Launcher.java
+++ ClientApp/src/All_Client/Launcher.java
@@ -7,28 +7,25 @@ import java.util.ResourceBundle;
 
 public class Launcher {
 
-	private static Socket socket = null;
-	
-	public static void main(String[] args) {
-		
-		//Get properties information
-		ResourceBundle bundle = ResourceBundle.getBundle("domaine.properties.config");
+  private static Socket socket = null;
 
-        String IP_adress = bundle.getString("server");
-        int PORT = Integer.parseInt(bundle.getString("port"));
-        
-		
-		//Try to create a new socket with IP and PORT
-		//If success then load IHM with socket in parameter
-		try {
-			socket = new Socket(IP_adress,PORT);
-			Interface_Client LeClient = new Interface_Client(socket);
-			LeClient.setVisible(true);
-			
-		} catch (UnknownHostException e) {
-			javax.swing.JOptionPane.showMessageDialog(null,"Impossible de se connecter à l'adresse "+socket.getLocalAddress(), "Alerte", javax.swing.JOptionPane.ERROR_MESSAGE);
-		} catch (IOException e) {
-			javax.swing.JOptionPane.showMessageDialog(null,"Aucun serveur à l'écoute du port ", "Alerte", javax.swing.JOptionPane.ERROR_MESSAGE);
-		}
-	}
+  public static void main(String[] args) {
+    //Get properties information
+    ResourceBundle bundle = ResourceBundle.getBundle("domaine.properties.config");
+    String IP_adress = bundle.getString("server");
+    int PORT = Integer.parseInt(bundle.getString("port"));
+
+    //Try to create a new socket with IP and PORT
+    //If success then load IHM with socket in parameter
+    try {
+      socket = new Socket(IP_adress,PORT);
+      Interface_Client LeClient = new Interface_Client(socket);
+      LeClient.setVisible(true);
+
+    } catch (UnknownHostException e) {
+      javax.swing.JOptionPane.showMessageDialog(null,"Impossible de se connecter à l'adresse "+socket.getLocalAddress(), "Alerte", javax.swing.JOptionPane.ERROR_MESSAGE);
+    } catch (IOException e) {
+      javax.swing.JOptionPane.showMessageDialog(null,"Aucun serveur à l'écoute du port ", "Alerte", javax.swing.JOptionPane.ERROR_MESSAGE);
+    }
+  }
 }
