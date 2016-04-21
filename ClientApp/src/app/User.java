85ee23488fa99effa02b5be2b30a10c6ce1101b3
redery <ederyruben@yahoo.fr>
2016-04-21 19:42:52 +0200

    [ClientApp] Rename constructor params

--- ClientApp/src/app/User.java
+++ ClientApp/src/app/User.java
@@ -5,9 +5,9 @@ public class User {
   private String login;
   private String pwd;
 
-  public User(String n, String a) {
-      login = n;
-      pwd = a;
+  public User(String login, String pwd) {
+      this.login = login;
+      this.pwd = pwd;
   }
 
   public String getLogin()  {
