89165b1aceb3767595bbc9cb27b4bf1a07d5582e
tarik <tarik.kich@navendis.com>
2016-04-25 20:19:08 +0200

    fix compile script

--- compile.sh
+++ compile.sh
@@ -7,8 +7,8 @@ printf "\33[32;5;32m%s\33[0m\n" "done!"
 
 #client compilation
 printf "\33[38;5;190m%s\33[0m\n" "client compilation..."
-javac -encoding ISO-8859-1 -d buildClient -cp "buildClient:ClientApp/lib/*" ClientApp/src/app/helpers/*.java
 javac -encoding ISO-8859-1 -d buildClient -cp "buildClient:ClientApp/lib/*" ClientApp/src/app/models/*.java
+javac -encoding ISO-8859-1 -d buildClient -cp "buildClient:ClientApp/lib/*" ClientApp/src/app/helpers/*.java
 javac -encoding ISO-8859-1 -d buildClient -cp "buildClient:ClientApp/lib/*" ClientApp/src/app/controllers/*.java
 javac -encoding ISO-8859-1 -d buildClient -cp "buildClient:ClientApp/lib/*" ClientApp/src/app/views/welcome/*.java
 javac -encoding ISO-8859-1 -d buildClient -cp "buildClient:ClientApp/lib/*" ClientApp/src/app/*.java
