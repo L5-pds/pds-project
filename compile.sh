a2d94ac70ac3cc4742102bcf372597aa04a18e9d
tarik <tarik.kich@hotmail.fr>
2016-05-24 01:14:44 +0200

    update compile script

--- compile.sh
+++ compile.sh
@@ -3,5 +3,5 @@
 #server & client compilation
 mkdir build
 printf "\33[38;5;190m%s\33[0m\n" "start compilation..."
-javac -encoding ISO-8859-1 -d build -cp "build:pdsApp/lib/*" pdsApp/src/app/listeners/*.java pdsApp/src/app/models/*.java pdsApp/src/app/models/component/*.java pdsApp/src/app/helpers/*.java pdsApp/src/app/controllers/*.java pdsApp/src/app/views/**/*.java pdsApp/src/app/*.java
+javac -encoding ISO-8859-1 -d build -cp "build:pdsApp/lib/*" pdsApp/src/app/listeners/*.java pdsApp/src/app/models/*.java pdsApp/src/app/models/**/*.java pdsApp/src/app/helpers/*.java pdsApp/src/app/controllers/*.java pdsApp/src/app/views/**/*.java pdsApp/src/app/*.java
 printf "\33[32;5;32m%s\33[0m\n" "done!"
