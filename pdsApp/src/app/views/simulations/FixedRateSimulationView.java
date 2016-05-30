0b4572d985b54d8d6168e922f08da8a0ad78bb73
agengoul <alexandre.gengoul@gmail.com>
2016-05-30 09:45:14 +0200

    ferf('

--- pdsApp/src/app/views/simulations/FixedRateSimulationView.java
+++ pdsApp/src/app/views/simulations/FixedRateSimulationView.java
@@ -65,6 +65,8 @@ public class FixedRateSimulationView {
         // assign a controller to the view
         controller = c;
         
+        System.out.println("fefe");
+        
         // configure the GridBagConstraints
         gc = new GridBagConstraints();
         gc.weightx = 0;
